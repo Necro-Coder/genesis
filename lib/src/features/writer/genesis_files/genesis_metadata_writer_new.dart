5c0201ead07db88ff557a75c9c700e34bd64bbcf
= <=>
2024-01-24 18:19:07 +0100

    Reading the json, writing metadata

--- lib/src/features/writer/genesis_files/genesis_metadata_writer_new.dart
+++ lib/src/features/writer/genesis_files/genesis_metadata_writer_new.dart
@@ -42,18 +42,34 @@ class MetadataManager {
       screens: GScreenList().commons.map((e) => e).toList(),
     );
 
-    GMetadataModel newMetadataModel =
-        await _compareLists(generalFiles, metadataModel.changes);
+    GMetadataModel newMetadataModel = await _checkForDeletions(
+        generalFiles,
+        metadataModel.changes,
+        metadataModel.unchanged,
+        await _compareLists(
+            generalFiles, metadataModel.changes, metadataModel.unchanged));
     if (_firstWrite) {
       newMetadataModel.changes = generalFiles;
-      newMetadataModel.deletions = GeneralFile();
-      newMetadataModel.unchanged = GeneralFile();
+      newMetadataModel.deletions = GeneralFile(
+        files: [],
+        folders: [],
+        properties: [],
+        widgets: [],
+        screens: [],
+      );
+      newMetadataModel.unchanged = GeneralFile(
+        files: [],
+        folders: [],
+        properties: [],
+        widgets: [],
+        screens: [],
+      );
     }
     await _writeMetadata(newMetadataModel);
   }
 
-  Future<GMetadataModel> _compareLists(
-      GeneralFile generalFiles, GeneralFile item) async {
+  Future<GMetadataModel> _compareLists(GeneralFile generalFiles,
+      GeneralFile itemChanges, GeneralFile itemUnchanged) async {
     GMetadataModel newMetadataModel = GMetadataModel(
       changes: GeneralFile(
         files: [],
@@ -78,52 +94,105 @@ class MetadataManager {
       ),
     );
 
-    if (item.files == null) {
+    if (itemChanges.files == null) {
       return newMetadataModel;
     }
-    stdout.writeln(
-        '${item.files}, ${item.folders}, ${item.properties}, ${item.widgets}, ${item.screens}');
-    for (var file in generalFiles.files!) {
-      if (!item.files!.contains(file)) {
-        newMetadataModel.changes.files!.add(file);
-      } else {
-        newMetadataModel.unchanged.files!.add(file);
-      }
-    }
 
-    for (var folder in generalFiles.folders!) {
-      if (!item.folders!.contains(folder)) {
-        newMetadataModel.changes.folders!.add(folder);
-      } else {
-        newMetadataModel.unchanged.folders!.add(folder);
-      }
-    }
-
-    for (var property in generalFiles.properties!) {
-      if (!item.properties!.contains(property)) {
-        newMetadataModel.changes.properties!.add(property);
-      } else {
-        newMetadataModel.unchanged.properties!.add(property);
-      }
-    }
-
-    for (var widget in generalFiles.widgets!) {
-      if (!item.widgets!.contains(widget)) {
-        newMetadataModel.changes.widgets!.add(widget);
-      } else {
-        newMetadataModel.unchanged.widgets!.add(widget);
-      }
-    }
-
-    for (var screen in generalFiles.screens!) {
-      if (!item.screens!.contains(screen)) {
-        newMetadataModel.changes.screens!.add(screen);
-      } else {
-        newMetadataModel.unchanged.screens!.add(screen);
-      }
-    }
+    _compareAndAddToLists(
+        generalFiles.files,
+        itemChanges.files,
+        itemUnchanged.files,
+        newMetadataModel.changes.files,
+        newMetadataModel.unchanged.files);
+    _compareAndAddToLists(
+        generalFiles.folders,
+        itemChanges.folders,
+        itemUnchanged.folders,
+        newMetadataModel.changes.folders,
+        newMetadataModel.unchanged.folders);
+    _compareAndAddToLists(
+        generalFiles.properties,
+        itemChanges.properties,
+        itemUnchanged.properties,
+        newMetadataModel.changes.properties,
+        newMetadataModel.unchanged.properties);
+    _compareAndAddToLists(
+        generalFiles.widgets,
+        itemChanges.widgets,
+        itemUnchanged.widgets,
+        newMetadataModel.changes.widgets,
+        newMetadataModel.unchanged.widgets);
+    _compareAndAddToLists(
+        generalFiles.screens,
+        itemChanges.screens,
+        itemUnchanged.screens,
+        newMetadataModel.changes.screens,
+        newMetadataModel.unchanged.screens);
 
     return newMetadataModel;
+  }
+
+  Future<GMetadataModel> _checkForDeletions(
+      GeneralFile generalFiles,
+      GeneralFile changes,
+      GeneralFile unchanged,
+      GMetadataModel newMetadataModel) async {
+    await _checkListForDeletions(generalFiles.files, changes.files,
+        unchanged.files, newMetadataModel.deletions.files);
+
+    await _checkListForDeletions(generalFiles.folders, changes.folders,
+        unchanged.folders, newMetadataModel.deletions.folders);
+
+    await _checkListForDeletions(generalFiles.properties, changes.properties,
+        unchanged.properties, newMetadataModel.deletions.properties);
+
+    await _checkListForDeletions(generalFiles.widgets, changes.widgets,
+        unchanged.widgets, newMetadataModel.deletions.widgets);
+
+    await _checkListForDeletions(generalFiles.screens, changes.screens,
+        unchanged.screens, newMetadataModel.deletions.screens);
+
+    return newMetadataModel;
+  }
+
+  Future<void> _checkListForDeletions(
+      List<dynamic>? generalList,
+      List<dynamic>? changesList,
+      List<dynamic>? unchangedList,
+      List<dynamic>? deletionsList) async {
+    if (generalList != null && changesList != null && unchangedList != null) {
+      for (var element in changesList) {
+        if (!generalList.contains(element)) {
+          deletionsList!.add(element);
+        }
+      }
+      for (var element in unchangedList) {
+        if (!generalList.contains(element)) {
+          deletionsList!.add(element);
+        }
+      }
+    }
+  }
+
+  void _compareAndAddToLists(
+      List<dynamic>? generalList,
+      List<dynamic>? itemListChanges,
+      List<dynamic>? itemListUnchanged,
+      List<dynamic>? changesList,
+      List<dynamic>? unchangedList) {
+    if (generalList != null &&
+        itemListChanges != null &&
+        changesList != null &&
+        unchangedList != null) {
+      for (var element in generalList) {
+        if (!itemListChanges.contains(element) &&
+            !itemListUnchanged!.contains(element)) {
+          changesList.add(element);
+        } else {
+          unchangedList.add(element);
+        }
+      }
+    }
   }
 
   Future<void> _writeMetadata(GMetadataModel metadataModel) async {
